3529cb16c41d853d9b891092e41f15f1208eba70
愛朱歌 <asyuka.engineer@gmail.com>
2023-04-01 01:57:15 +0900

    I created service button but it's on the way

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,7 +1,9 @@
-
+import ServiceButton from './subcomponents/ServiceButton'
 const Services = () => {
+
+
   return (
-    <section className="my-12 mx-auto px-4 text-center tracking-widest leading-relaxed font-sans font-light text-gray-800">
+    <section className="relative my-12 mx-auto px-4 text-center tracking-widest leading-relaxed font-sans font-light text-gray-800">
       <h3 className="uppercase relative font-bold text-3xl before:relative before:inline-block before:h-8 before:mx-8 before:border-l-[3px] before:border-emerald-500 before:rotate-[-35deg] after:relative after:inline-block after:h-8 after:mx-8 after:border-r-[3px] after:border-emerald-500 after:rotate-[35deg]">
         Services
       </h3>
@@ -11,8 +13,9 @@ const Services = () => {
         <br />
         お客様の良き理解者としてビジネスを支援していきます。
       </p>
+      <ServiceButton />
     </section>
-    
+
   )
 }
 
---
+++ components/subcomponents/ServiceButton.tsx
@@ -0,0 +1,13 @@
+
+const ServiceButton = () => {
+  return (
+    <div>
+     <button className="bg-neutral-100">
+       <p>ECサイト構築</p>
+       <p>E-commerce</p>
+    </button>
+    </div>
+  )
+}
+
+export default ServiceButton
