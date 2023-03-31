49421208b789a2ea2b12475761c0928caa1e44e8
愛朱歌 <asyuka.engineer@gmail.com>
2023-03-31 18:42:43 +0900

    The h3 services title has been decorated with green borders on both sides

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,26 +6,27 @@ const Hero = () => {
   return (
     <div className="mx-auto px-4 text-center tracking-widest leading-relaxed font-sans font-light text-gray-800">
       {/* hero title */}
-        <h2 className="w-auto my-8 uppercase text-3xl">
+        <h2 className="w-auto mt-8 uppercase text-3xl">
           create our future
           <br/>
             with technology
-          <br/>
-          <span className="text-base">
-          創造的な未来をテクノロジーと共に。
-          </span>
         </h2>   
+          <p className="text-base mb-8">
+          創造的な未来をテクノロジーと共に。
+          </p>
       {/* hero image */}
       <Image src={heroSvg} alt="hero image" className="mx-auto lg:max-w-3xl"/> 
       {/* about */}
       <section className="my-12">
         <p className="text-xl mb-6">
           Companyは最先端技術を駆使して
-          <br/>リッチなWeb体験をプロデュースします。
+          <br/>
+          リッチなWeb体験をプロデュースします。
         </p>
         <p className="mb-8">
           Web。そこは、顧客と商品が出会い、関係を構築していく場所。
-          <br/>Webテクノロジーとビジネスの専門家集団である私たちが、総合的なユーザー体験をプロデュースします。
+          <br/>
+          Webテクノロジーとビジネスの専門家集団である私たちが、総合的なユーザー体験をプロデュースします。
         </p>
         <Link href="../about">
           <button className="w-56 p-3 bg-emerald-500 hover:bg-emerald-300 text-white rounded-3xl duration-300">
--- components/Services.tsx
+++ components/Services.tsx
@@ -2,12 +2,17 @@
 const Services = () => {
   return (
     <section className="my-12 mx-auto px-4 text-center tracking-widest leading-relaxed font-sans font-light text-gray-800">
-      <h3 className="uppercase text-3xl">
+      <h3 className="uppercase relative font-bold text-3xl before:relative before:inline-block before:h-8 before:mx-8 before:border-l-[3px] before:border-emerald-500 before:rotate-[-35deg] after:relative after:inline-block after:h-8 after:mx-8 after:border-r-[3px] after:border-emerald-500 after:rotate-[35deg]">
         Services
-        <br/>
-        <span className="text-xl">私たちにできること</span>
       </h3>
+      <p className="mb-6">私たちにできること</p>
+      <p>
+        私たちのクリエイティブな強みを活かしたWebソリューションの提供により、
+        <br />
+        お客様の良き理解者としてビジネスを支援していきます。
+      </p>
     </section>
+    
   )
 }
 
