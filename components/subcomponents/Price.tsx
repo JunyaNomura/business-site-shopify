92b69a796280e71187ab74361f0b6387abf93799
愛朱歌 <asyuka.engineer@gmail.com>
2023-04-17 16:57:05 +0900

    I tried to loop the price-plan's content

--- components/subcomponents/Price.tsx
+++ components/subcomponents/Price.tsx
@@ -2,6 +2,14 @@
 
 const Price = () => {
   const planTitle = "LIGHT"
+  const recommendations1 = [
+    "ブランディングを図りたい",
+    "サイトに載せる写真を撮影込みでお願いしたい",
+    "デザインにこだわりたい",
+  ]
+  const recommentation2 = [
+    
+  ]
   
   return (
 
@@ -15,14 +23,23 @@ const Price = () => {
           </span>
         </p>  
       </div>
-      <div className="w-full bg-neutral-100 py-4 px-3 flex">
+      <div className="w-full bg-neutral-100 py-4 px-3 flex gap-2">
         <p className="w-1/3 text-center">
          こんな方におすすめ
         </p>
         <p className="w-2/3 break-words">
+          {/* 料金 */}
           <span className="text-xl block">¥100000〜</span>
+          {/* 説明 */}
           <span className="text-sm block"></span>
-          <span className="text-sm block"></span>
+          {/* 箇条書き */}
+          {recommendations1 ? recommendations1.map((recommendation) => 
+            <ul className="text-sm block">
+              <li className="before:content-['・']">{recommendation}</li>
+            </ul>
+          ) : <span></span>
+          }
+          {/* 注釈 */}
           <span className="text-xs block"></span>
         </p>
       </div>
